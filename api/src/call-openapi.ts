fa347c3b3a4ef7ef98e562a1214ca982ca916557
Yuko Oba <chambao752@gmail.com>
2024-09-05 23:01:08 +0900

    update: use mini model as request exceeds tokens per min

--- api/src/call-openapi.ts
+++ api/src/call-openapi.ts
@@ -4,9 +4,9 @@ const openai = new OpenAI();
 
 export const callOpenAI = async (base64: string) => {
   const stream = await openai.chat.completions.create({
-    model: 'gpt-4o',
+    model: 'gpt-4o-mini',
     messages: [
-      { role: 'user', content: "画像からキーと数値を読み込んで、Jsonデータとして返却してください。" },
+      { role: 'user', content: "画像からカロリー、たんぱく質、炭水化物、などをキーとして、実際の摂取量の数値を読み取り、Jsonデータとして返却してください。" },
       { role: 'user', content:  `data:image/jpeg;base64,${base64}`}
     ],
     stream: true,
